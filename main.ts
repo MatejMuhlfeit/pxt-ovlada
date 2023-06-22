eb819827e1a2a088f813564119fd7b53f89e4f34
MatejMuhlfeit <115697262+MatejMuhlfeit@users.noreply.github.com>
2023-06-22 09:21:37 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -26,7 +26,7 @@ function calculateMotorSpeeds(xAcceleration: number, yAcceleration: number) {
 
 basic.forever(function () {
     let xAcceleration = input.acceleration(Dimension.X);
-    let yAcceleration = input.acceleration(Dimension.Y);
+    let yAcceleration = input.acceleration(Dimension.Z);
 
     let [leftMotorSpeed, rightMotorSpeed] = calculateMotorSpeeds(xAcceleration, yAcceleration);
 
